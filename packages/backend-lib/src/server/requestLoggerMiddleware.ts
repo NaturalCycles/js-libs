4f351e0b1e6b78fb07bd48b01b1cabb4576d344e
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2025-06-05 03:07:59 +0200

    feat: requestLoggerMiddleware to support url prefix trimming

--- packages/backend-lib/src/server/requestLoggerMiddleware.ts
+++ packages/backend-lib/src/server/requestLoggerMiddleware.ts
@@ -3,20 +3,37 @@ import { _since } from '@naturalcycles/js-lib'
 import type { BackendRequestHandler } from '../index.js'
 import { onFinished } from '../index.js'
 
+export interface RequestLoggerMiddlewareCfg {
+  /**
+   * If set - this prefix will be removed from the request url before logging.
+   */
+  removeUrlPrefix?: string
+}
+
 /**
  * Experimental request logger for Cloud Run.
  *
  * @experimental
  */
-export function requestLoggerMiddleware(): BackendRequestHandler {
+export function requestLoggerMiddleware(
+  cfg: RequestLoggerMiddlewareCfg = {},
+): BackendRequestHandler {
+  const { removeUrlPrefix } = cfg
+  const removeUrlPrefixLength = removeUrlPrefix?.length
+
   return (req, res, next) => {
     const started = Date.now() as UnixTimestampMillis
 
+    let url = req.originalUrl.split('?')[0]!
+    if (removeUrlPrefix && url.startsWith(removeUrlPrefix)) {
+      url = url.slice(removeUrlPrefixLength)
+    }
+
     // todo: include requestId (3-character hash of it?)
-    req.log(['>>', req.method, req.originalUrl, req.userId].filter(Boolean).join(' '))
+    req.log(['>>', req.method, url, req.userId].filter(Boolean).join(' '))
 
     onFinished(res, () => {
-      const str = ['<<', res.statusCode, _since(started), req.method, req.originalUrl, req.userId]
+      const str = ['<<', res.statusCode, _since(started), req.method, url, req.userId]
         .filter(Boolean)
         .join(' ')
 
