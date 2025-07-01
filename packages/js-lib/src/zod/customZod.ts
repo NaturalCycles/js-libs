534042fc2400333ca8df6273aa244d61b489e2c0
David Nagy <mrnagydavid@users.noreply.github.com>
2025-07-01 18:35:45 +0200

    fix: re-add `ZodType`

--- packages/js-lib/src/zod/customZod.ts
+++ packages/js-lib/src/zod/customZod.ts
@@ -1,9 +1,11 @@
-import z4 from 'zod/v4'
+import { z as z4, ZodType } from 'zod/v4'
 import { customZodSchemas } from './zod.shared.schemas.js'
 
 type ExtendedZod = Omit<typeof z4, keyof typeof customZodSchemas | 'iso'> & typeof customZodSchemas
 
-export const z: ExtendedZod = { ...z4, ...customZodSchemas }
+const z: ExtendedZod = { ...z4, ...customZodSchemas }
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export type zInfer<T> = z4.infer<T>
+type zInfer<T> = z4.infer<T>
+
+export { z, type zInfer, ZodType }
