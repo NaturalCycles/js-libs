8c4aab245238ac171c6bc8075bda60a4f97093c5
Carl Anthon Wellsjö <anthon.wellsjo@naturalcycles.com>
2025-07-07 09:10:12 +0200

    feat(ci): support gae

--- packages/backend-lib/src/server/serverStatusMiddleware.ts
+++ packages/backend-lib/src/server/serverStatusMiddleware.ts
@@ -1,23 +1,43 @@
 import { _filterNullishValues, localTime } from '@naturalcycles/js-lib'
 import { memoryUsageFull, processSharedUtil } from '@naturalcycles/nodejs-lib'
+import { getDeployInfo } from './deployInfo.util.js'
 import type { BackendRequestHandler } from './server.model.js'
 
 const { versions, arch, platform } = process
-const { GOOGLE_CLOUD_PROJECT, K_SERVICE, K_REVISION, APP_ENV, NODE_OPTIONS, DEPLOY_BUILD_TIME } =
-  process.env
+const {
+  GAE_APPLICATION,
+  GAE_SERVICE,
+  GAE_VERSION,
+  GOOGLE_CLOUD_PROJECT,
+  K_SERVICE,
+  K_REVISION,
+  APP_ENV,
+  NODE_OPTIONS,
+  DEPLOY_BUILD_TIME,
+} = process.env
 
-export function serverStatusMiddleware(extra?: any): BackendRequestHandler {
+export function serverStatusMiddleware(projectDir?: string, extra?: any): BackendRequestHandler {
   return async (_req, res) => {
-    res.json(getServerStatusData(extra))
+    res.json(getServerStatusData(projectDir, extra))
   }
 }
 
-export function getServerStatusData(extra?: any): Record<string, any> {
+export function getServerStatusData(
+  projectDir: string = process.cwd(),
+  extra?: any,
+): Record<string, any> {
+  const { ts } = getDeployInfo(projectDir)
+  const t = localTime(ts)
+  const deployBuildTime = DEPLOY_BUILD_TIME || t.toPretty()
+
   return _filterNullishValues({
-    nodeProcessStarted: getStartedStr(),
-    DEPLOY_BUILD_TIME,
+    started: getStartedStr(),
+    deployBuildTime,
     APP_ENV,
     GOOGLE_CLOUD_PROJECT,
+    GAE_APPLICATION,
+    GAE_SERVICE,
+    GAE_VERSION,
     K_SERVICE,
     K_REVISION,
     processInfo: {
