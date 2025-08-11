550de49805cb2dd7d23e6e9006a94282e8b3e9d0
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2025-08-11 08:00:45 +0200

    feat: `getRequestEndpoint` to support `stripPrefix`

--- packages/backend-lib/src/server/request.util.ts
+++ packages/backend-lib/src/server/request.util.ts
@@ -7,11 +7,18 @@ import type { BackendRequest } from './server.model.js'
  *
  * Gets the correct full path when used from sub-router-resources.
  * Strips away the queryString.
+ *
+ * If stripPrefix (e.g `/api/v2`) is provided, and the path starts with it (like path.startsWith(stripPrefix)),
+ * it will be stripped from the beginning of the path.
  */
-export function getRequestEndpoint(req: BackendRequest): string {
+export function getRequestEndpoint(req: BackendRequest, stripPrefix?: string): string {
   let path = (req.baseUrl + (req.route?.path || req.path)).toLowerCase()
   if (path.length > 1 && path.endsWith('/')) {
     path = path.slice(0, path.length - 1)
+  }
+
+  if (stripPrefix && path.startsWith(stripPrefix)) {
+    path = path.slice(stripPrefix.length)
   }
 
   return [req.method, path].join(' ')
