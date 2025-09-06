77bf2f9c7d81916afc424a21a43d8e1be728014d
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2025-09-06 14:53:49 +0200

    feat: refine query util

--- packages/firestore-lib/src/query.util.ts
+++ packages/firestore-lib/src/query.util.ts
@@ -1,4 +1,4 @@
-import type { Query, WhereFilterOp } from '@google-cloud/firestore'
+import { FieldPath, type Query, type WhereFilterOp } from '@google-cloud/firestore'
 import type { DBQuery, DBQueryFilterOperator } from '@naturalcycles/db-lib'
 import type { ObjectWithId } from '@naturalcycles/js-lib/types'
 
@@ -17,14 +17,12 @@ export function dbQueryToFirestoreQuery<ROW extends ObjectWithId>(
 
   // filter
   for (const f of dbQuery._filters) {
-    q = q.where(f.name as string, OP_MAP[f.op] || (f.op as WhereFilterOp), f.val)
+    q = q.where(mapName(f.name), OP_MAP[f.op] || (f.op as WhereFilterOp), f.val)
   }
 
   // order
   for (const ord of dbQuery._orders) {
-    // todo: support ordering by id like this:
-    // .orderBy(FieldPath.documentId())
-    q = q.orderBy(ord.name as string, ord.descending ? 'desc' : 'asc')
+    q = q.orderBy(mapName(ord.name), ord.descending ? 'desc' : 'asc')
   }
 
   // limit
@@ -32,13 +30,18 @@ export function dbQueryToFirestoreQuery<ROW extends ObjectWithId>(
 
   // selectedFields
   if (dbQuery._selectedFieldNames) {
-    // todo: check if at least id / __key__ is required to be set
-    q = q.select(...(dbQuery._selectedFieldNames as string[]))
+    // id is filtered out, because in Firestore it's not a "property",
+    // and doc.id is always returned, even if we request empty set of fields
+    q = q.select(...(dbQuery._selectedFieldNames as string[]).filter(n => n !== 'id'))
   }
 
   // cursor
   if (dbQuery._startCursor) {
-    q = q.startAt(dbQuery._startCursor)
+    // Using `startAfter`, not `startAt` here
+    // Why?
+    // Because in Firestore, you can only retrieve "last document id" to be used as Cursor.
+    // That document was already retrieved, so it makes sense to start AFTER it.
+    q = q.startAfter(dbQuery._startCursor)
   }
 
   if (dbQuery._endCursor) {
@@ -46,4 +49,9 @@ export function dbQueryToFirestoreQuery<ROW extends ObjectWithId>(
   }
 
   return q
+}
+
+function mapName<ROW extends ObjectWithId>(name: keyof ROW): string | FieldPath {
+  if (name === 'id') return FieldPath.documentId()
+  return name as string
 }
