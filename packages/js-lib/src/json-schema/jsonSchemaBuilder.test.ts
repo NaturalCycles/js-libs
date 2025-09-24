c11dc4920ee3b40b8c0b8d1d2261f4de54efd912
David Nagy <mrnagydavid@users.noreply.github.com>
2025-09-24 16:16:45 +0200

    chore: add tests

--- packages/js-lib/src/json-schema/jsonSchemaBuilder.test.ts
+++ packages/js-lib/src/json-schema/jsonSchemaBuilder.test.ts
@@ -1,6 +1,6 @@
 import { AjvSchema } from '@naturalcycles/nodejs-lib/ajv'
-import { expect, test } from 'vitest'
-import type { BaseDBEntity, IsoDate, UnixTimestamp } from '../types.js'
+import { describe, expect, test } from 'vitest'
+import type { BaseDBEntity, Branded, IsoDate, UnixTimestamp } from '../types.js'
 import { z } from '../zod/index.js'
 import { j } from './jsonSchemaBuilder.js'
 import { baseDBEntityJsonSchema } from './jsonSchemas.js'
@@ -134,4 +134,117 @@ test.todo('compare with zod json schema', () => {
   zodJsonSchema.required!.sort() // for snapshot stability
 
   expect(jsonSchema).toEqual(zodJsonSchema)
+})
+
+describe('integer', () => {
+  describe('branded', () => {
+    type BPM = Branded<number, 'BPM'>
+
+    test('should assign the branded type', () => {
+      const schema = j.object({
+        foo: j.number().branded<BPM>(),
+      })
+      const [, result] = AjvSchema.create(schema.build()).getValidationResult({ foo: 1 as BPM })
+      result satisfies { foo: BPM }
+    })
+
+    test('should be chainable with other commands', () => {
+      const schema = j.object({
+        foo: j.number().branded<BPM>().max(2),
+      })
+
+      const [err, result] = AjvSchema.create(schema.build()).getValidationResult({
+        foo: 10 as BPM,
+      })
+
+      expect(err).toMatchInlineSnapshot(`
+        [AjvValidationError: Object/foo must be <= 2
+        Input: { foo: 10 }]
+      `)
+      expect(result).toEqual({ foo: 10 })
+      result satisfies { foo: BPM }
+    })
+  })
+})
+
+describe('string', () => {
+  describe('branded', () => {
+    type AccountId = Branded<string, 'AccountId'>
+
+    test('should assign the branded type', () => {
+      const schema = j.object({
+        foo: j.string().branded<AccountId>(),
+      })
+      const [, result] = AjvSchema.create(schema.build()).getValidationResult({
+        foo: 'bingbong' as AccountId,
+      })
+      result satisfies { foo: AccountId }
+    })
+
+    test('should be chainable with other commands', () => {
+      const schema = j.object({
+        foo: j.string().branded<AccountId>().max(2),
+      })
+
+      const [err, result] = AjvSchema.create(schema.build()).getValidationResult({
+        foo: 'bingbong' as AccountId,
+      })
+
+      expect(err).toMatchInlineSnapshot(`
+        [AjvValidationError: Object/foo must NOT have more than 2 characters
+        Input: { foo: 'bingbong' }]
+      `)
+      expect(result).toEqual({ foo: 'bingbong' })
+      result satisfies { foo: AccountId }
+    })
+  })
+})
+
+describe('array', () => {
+  test('should correctly infer the type from the type of the items', () => {
+    const schema = j.object({ foo: j.array(j.number()) })
+    const [, result] = AjvSchema.create(schema.build()).getValidationResult({ foo: 1 })
+    result satisfies { foo: number[] }
+  })
+})
+
+describe('optional', () => {
+  test('should correctly infer the type of optional fields', () => {
+    interface Foo {
+      reqNum: number
+      optNum?: number | undefined
+      reqStr: string
+      optStr?: string | undefined
+      // TODO: uncomment when `j.array()` is fixed
+      // reqArrOfReqNum: number[]
+      // reqArrOfOptNum: (number | undefined)[]
+      // optArrOfReqNum?: number[] | undefined
+      // optArrOfOptNum?: (number | undefined)[] | undefined
+    }
+    const schema = j.object({
+      reqNum: j.number(),
+      optNum: j.number().optional(),
+      reqStr: j.string(),
+      optStr: j.string().optional(),
+      // reqArrOfReqNum: j.array(j.number()),
+      // reqArrOfOptNum: j.array(j.number().optional()),
+      // optArrOfReqNum: j.array(j.number()).optional(),
+      // optArrOfOptNum: j.array(j.number().optional()).optional(),
+    })
+    const badSchema = j.object({
+      reqNum: j.number().optional(),
+      optNum: j.number(),
+      reqStr: j.string().optional(),
+      optStr: j.string(),
+    })
+
+    const [, result] = AjvSchema.create(schema.build()).getValidationResult({} as any)
+
+    result satisfies Foo
+
+    const [, resultOfBadSchema] = AjvSchema.create(schema.build()).getValidationResult({} as any)
+
+    // @ts-expect-error
+    resultOfBadSchema satisfies Foo
+  })
 })
