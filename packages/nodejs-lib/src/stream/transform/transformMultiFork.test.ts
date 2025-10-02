90755db5acdf5b4b695fb8a7fd0d0e9fa2befb09
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2025-10-02 23:10:06 +0200

    feat: transformMultiFork (alternative to writableChunk)

--- packages/nodejs-lib/src/stream/transform/transformMultiFork.test.ts
+++ packages/nodejs-lib/src/stream/transform/transformMultiFork.test.ts
@@ -2,25 +2,28 @@ import { _range } from '@naturalcycles/js-lib/array'
 import type { Predicate } from '@naturalcycles/js-lib/types'
 import { expect, test } from 'vitest'
 import { Pipeline } from '../pipeline.js'
-import { writableChunk } from './writableChunk.js'
-import { writablePushToArray } from './writablePushToArray.js'
+import { transformMultiFork } from './transformMultiFork.js'
 
-test('writableChunk', async () => {
+test('transformMultiFork', async () => {
   const splitPredicate: Predicate<number> = (_, i) => {
     return i % 3 === 0
   }
   const arrays: number[][] = []
 
-  await Pipeline.fromArray(_range(10)).to(
-    writableChunk({
-      splitPredicate,
-      writableFactory: _splitIndex => {
-        const array: number[] = []
-        arrays.push(array)
-        return writablePushToArray(array)
-      },
-    }),
-  )
+  await Pipeline.fromArray(_range(10))
+    .transform(
+      transformMultiFork(
+        splitPredicate,
+        async pipeline => {
+          const array = await pipeline.toArray()
+          arrays.push(array)
+        },
+        {
+          logLevel: 'debug',
+        },
+      ),
+    )
+    .run()
 
   expect(arrays).toMatchInlineSnapshot(`
     [
