a48a90f76f1d0f32ebb3d32c105d23683a07d267
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2025-10-02 19:00:26 +0200

    chore: review

--- packages/nodejs-lib/src/stream/writable/writableChunk.ts
+++ packages/nodejs-lib/src/stream/writable/writableChunk.ts
@@ -1,20 +1,90 @@
 import { Writable } from 'node:stream'
-import { promisify } from 'node:util'
-import { _last } from '@naturalcycles/js-lib/array'
+import { _first, _last } from '@naturalcycles/js-lib/array'
+import { createCommonLoggerAtLevel } from '@naturalcycles/js-lib/log'
 import { _deepCopy } from '@naturalcycles/js-lib/object'
+import type { Predicate } from '@naturalcycles/js-lib/types'
 import {
   transformNoOp,
   type TransformOptions,
   type TransformTyped,
   type WritableTyped,
-} from '@naturalcycles/nodejs-lib/stream'
+} from '../index.js'
 
-// This is a helper function to create a promise which resolves when the stream emits a 'finish'
-// event.
-// This is used to await all the writables in the final method of the writableChunk
-async function awaitFinish(stream: Writable): Promise<unknown> {
-  /* eslint-disable-next-line no-extra-bind */
-  return await promisify(((cb: any) => stream.on('finish', cb)).bind(stream))()
+export interface WritableChunkOptions<T> extends TransformOptions {
+  splitPredicate: Predicate<T>
+  transformFactories?: (() => TransformTyped<T, T>)[]
+  writableFactory: (splitIndex: number) => WritableTyped<T>
+}
+
+/**
+ * Allows to split the output to multiple files by splitting into chunks
+ * based on `shouldSplitFn`.
+ * `transformFactories` are used to create a chain of transforms for each chunk.
+ * It was meant to be used with createGzip, which needs a proper start and end for each chunk
+ * for the output file to be a valid gzip file.
+ */
+export function writableChunk<T>(opt: WritableChunkOptions<T>): WritableTyped<T> {
+  const { highWaterMark, splitPredicate, transformFactories = [], writableFactory } = opt
+  const logger = createCommonLoggerAtLevel(opt.logger, opt.logLevel)
+
+  let indexWritten = 0
+  let currentSplitIndex = 0
+  // We don't want to have an empty chain, so we add a no-op transform
+  if (transformFactories.length === 0) {
+    transformFactories.push(transformNoOp<T>)
+  }
+
+  // Create the transforms as well as the Writable, and pipe them together
+  let currentWritable = writableFactory(currentSplitIndex)
+  let transforms = transformFactories.map(f => f())
+  generateTuples(transforms).forEach(([t1, t2]) => t1.pipe(t2))
+  _last(transforms).pipe(currentWritable)
+
+  // We keep track of all the pending writables, so we can await them in the final method
+  const writablesFinish: Promise<void>[] = [awaitFinish(currentWritable)]
+
+  return new Writable({
+    objectMode: true,
+    highWaterMark,
+    write(chunk: T, _, cb) {
+      // pipe will take care of piping the data through the different streams correctly
+      transforms[0]!.write(chunk, cb)
+
+      if (splitPredicate(chunk, ++indexWritten)) {
+        logger.log(`writableChunk: splitting at index ${currentSplitIndex}`)
+        currentSplitIndex++
+        transforms[0]!.end()
+
+        currentWritable = writableFactory(currentSplitIndex)
+        transforms = transformFactories.map(f => f())
+        generateTuples(transforms).forEach(([t1, t2]) => t1.pipe(t2))
+        _last(transforms).pipe(currentWritable)
+
+        writablesFinish.push(awaitFinish(currentWritable))
+      }
+    },
+    async final(cb) {
+      try {
+        transforms[0]!.end()
+        await Promise.all(writablesFinish)
+        logger.log('writableChunk: all writables are finished')
+        cb()
+      } catch (err) {
+        cb(err as Error)
+      }
+    },
+  })
+}
+
+/**
+ * This is a helper function to create a promise which resolves when the stream emits a 'finish'
+ * event.
+ * This is used to await all the writables in the final method of the writableChunk
+ */
+async function awaitFinish(stream: Writable): Promise<void> {
+  return await new Promise(resolve => {
+    stream.on('finish', resolve)
+  })
 }
 
 /**
@@ -31,65 +101,4 @@ function generateTuples<T>(arr: T[]): [T, T][] {
     tuples.push([arrCopy[i - 1]!, arrCopy[i]!])
   }
   return tuples
-}
-
-/**
- * Allows to split the output to multiple files by splitting into chunks
- * based on `shouldSplitFn`.
- * `transformFactories` are used to create a chain of transforms for each chunk.
- * It was meant to be used with createGzip, which needs a proper start and end for each chunk
- * for the output file to be a valid gzip file.
- */
-export function writableChunk<T>(
-  shouldSplitFn: (row: T) => boolean,
-  transformFactories: (() => TransformTyped<T, T>)[],
-  writableFactory: (index: number) => WritableTyped<T>,
-  opt?: TransformOptions,
-): WritableTyped<T> {
-  let currentSplitIndex = 0
-  // We don't want to have an empty chain, so we add a no-op transform
-  if (transformFactories.length === 0) {
-    transformFactories.push(transformNoOp<T>)
-  }
-
-  // Create the transforms as well as the Writable, and pipe them together
-  let currentWritable = writableFactory(currentSplitIndex)
-  let transforms = transformFactories.map(f => f())
-  generateTuples(transforms).forEach(([t1, t2]) => t1.pipe(t2))
-  _last(transforms).pipe(currentWritable)
-
-  // We keep track of all the pending writables, so we can await them in the final method
-  const writablesFinish: Promise<unknown>[] = [awaitFinish(currentWritable)]
-
-  return new Writable({
-    objectMode: true,
-    ...opt,
-    write(chunk: T, _, cb) {
-      // pipe will take care of piping the data through the different streams correctly
-      transforms[0]!.write(chunk, cb)
-
-      if (shouldSplitFn(chunk)) {
-        console.log(`writableChunk splitting at index: ${currentSplitIndex}`)
-        currentSplitIndex++
-        transforms[0]!.end()
-
-        currentWritable = writableFactory(currentSplitIndex)
-        transforms = transformFactories.map(f => f())
-        generateTuples(transforms).forEach(([t1, t2]) => t1.pipe(t2))
-        _last(transforms).pipe(currentWritable)
-
-        writablesFinish.push(awaitFinish(currentWritable))
-      }
-    },
-    async final(cb) {
-      try {
-        transforms[0]!.end()
-        await Promise.all(writablesFinish)
-        console.log('All writables are finished')
-        cb()
-      } catch (err) {
-        cb(err as Error)
-      }
-    },
-  })
 }
