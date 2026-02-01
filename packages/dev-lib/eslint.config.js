03588143053ad7518d0933e20289374a4a9f1f0c
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2026-02-01 15:13:12 +0100

    chore: top running eslint, start running typecheck in CI

--- packages/dev-lib/eslint.config.js
+++ packages/dev-lib/eslint.config.js
@@ -1,5 +1,6 @@
 import sharedConfig from './cfg/eslint.config.js'
 
+// This file is left purely for the purpose of testing dev-lib's shared eslint config
 export default [
   ...sharedConfig,
   {
--- packages/firestore-lib/eslint.config.js
+++
@@ -1,8 +0,0 @@
-import sharedConfig from '@naturalcycles/dev-lib/cfg/eslint.config.js'
-
-export default [
-  ...sharedConfig,
-  {
-    rules: {},
-  },
-]
