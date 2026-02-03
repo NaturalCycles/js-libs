82f89e45b8430fc3735af6eb92fafdd557c7f51f
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2026-02-03 19:25:07 +0100

    chore: deps

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,11 @@
 import { prettierExtensionsAll } from './packages/dev-lib/cfg/_cnst.js'
-import { runActionlint, runBiomeOxlintOxfmt } from './packages/dev-lib/cfg/lint-staged.config.js'
+import {
+  runActionlintOxfmt,
+  runBiomeOxlintOxfmt,
+} from './packages/dev-lib/cfg/lint-staged.config.js'
 
 export default {
   [`./**/*.{${prettierExtensionsAll}}`]: runBiomeOxlintOxfmt,
 
-  './.github/**/*.{yml,yaml}': runActionlint,
+  './.github/**/*.{yml,yaml}': runActionlintOxfmt,
 }
