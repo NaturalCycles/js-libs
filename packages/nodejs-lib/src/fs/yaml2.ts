3b059433f24a29b0068b19200a6917493fa034d6
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2026-03-09 02:32:09 +0100

    feat: replace js-yaml with yaml

--- packages/nodejs-lib/src/fs/yaml2.ts
+++ packages/nodejs-lib/src/fs/yaml2.ts
@@ -1,35 +1,47 @@
 import fs from 'node:fs'
 import fsp from 'node:fs/promises'
-import type { DumpOptions } from 'js-yaml'
-import yaml from 'js-yaml'
+import type {
+  CreateNodeOptions,
+  DocumentOptions,
+  ParseOptions,
+  SchemaOptions,
+  ToStringOptions,
+} from 'yaml'
+import { parse, stringify } from 'yaml'
 import { fs2 } from './fs2.js'
+
+export type YamlStringifyOptions = DocumentOptions &
+  SchemaOptions &
+  ParseOptions &
+  CreateNodeOptions &
+  ToStringOptions
 
 class Yaml2 {
   readYaml<T = unknown>(filePath: string): T {
-    return yaml.load(fs.readFileSync(filePath, 'utf8')) as T
+    return parse(fs.readFileSync(filePath, 'utf8')) as T
   }
 
   async readYamlAsync<T = unknown>(filePath: string): Promise<T> {
-    return yaml.load(await fsp.readFile(filePath, 'utf8')) as T
+    return parse(await fsp.readFile(filePath, 'utf8')) as T
   }
 
-  writeYaml(filePath: string, data: any, opt?: DumpOptions): void {
-    const str = yaml.dump(data, opt)
+  writeYaml(filePath: string, data: any, opt?: YamlStringifyOptions): void {
+    const str = stringify(data, opt)
     fs.writeFileSync(filePath, str)
   }
 
-  async writeYamlAsync(filePath: string, data: any, opt?: DumpOptions): Promise<void> {
-    const str = yaml.dump(data, opt)
+  async writeYamlAsync(filePath: string, data: any, opt?: YamlStringifyOptions): Promise<void> {
+    const str = stringify(data, opt)
     await fsp.writeFile(filePath, str)
   }
 
-  outputYaml(filePath: string, data: any, opt?: DumpOptions): void {
-    const str = yaml.dump(data, opt)
+  outputYaml(filePath: string, data: any, opt?: YamlStringifyOptions): void {
+    const str = stringify(data, opt)
     fs2.outputFile(filePath, str)
   }
 
-  async outputYamlAsync(filePath: string, data: any, opt?: DumpOptions): Promise<void> {
-    const str = yaml.dump(data, opt)
+  async outputYamlAsync(filePath: string, data: any, opt?: YamlStringifyOptions): Promise<void> {
+    const str = stringify(data, opt)
     await fs2.outputFileAsync(filePath, str)
   }
 }
