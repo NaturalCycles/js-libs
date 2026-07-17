c230c23b98c9b9818a9fb06601d523918df8c6b8
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2026-07-17 22:28:24 +0200

    fix: pin oxlint-tsgolint to 0.23.0
    due to https://github.com/oxc-project/tsgolint/issues/1094

--- packages/dev-lib/vitest.config.ts
+++ packages/dev-lib/vitest.config.ts
@@ -2,10 +2,7 @@ import { defineVitestConfig } from './cfg/vitest.config.js'
 
 export default defineVitestConfig(
   {
-    test: {
-      // three
-      // bail: 1,
-    },
+    test: {},
   },
   import.meta.dirname,
 )
