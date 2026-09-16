35e447a63d8a64bfe298ed6dc96359ae5d3590f4
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2026-09-16 08:33:56 +0200

    chore: deps

--- packages/airtable-lib/src/airtableLib.test.ts
+++ packages/airtable-lib/src/airtableLib.test.ts
@@ -1,26 +1,5 @@
-import { AppError } from '@naturalcycles/js-lib/error/error.util.js'
 import { expect, test } from 'vitest'
-import { AirtableErrorCode } from './airtable.model.js'
 import { AirtableLib } from './airtableLib.js'
-import { mockTableDao1 } from './test/airtable.mock.js'
-
-test('wrong apiKey should throw', async () => {
-  const airtableLib = new AirtableLib({
-    apiKey: 'apiKey123',
-  })
-  const tableDao = mockTableDao1(airtableLib, 'someBaseId')
-
-  // await expect(airtableService.getRecords('someBaseId.someTable')).rejects.toThrow(AppError)
-  const err = await tableDao.getRecords().catch(err2 => err2)
-  expect(err).toBeInstanceOf(AppError)
-  expect(err).toMatchObject({
-    message: expect.any(String),
-    stack: expect.any(String),
-    data: {
-      code: AirtableErrorCode.AIRTABLE_ERROR,
-    },
-  })
-})
 
 test('api', async () => {
   const airtableLib = new AirtableLib({
